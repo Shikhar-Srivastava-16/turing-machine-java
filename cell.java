78f0b0819d21b92e4be703257374cdb8b7a10cc4
Shikhar Srivastava <109688678+Shikhar-Srivastava-16@users.noreply.github.com>
2023-12-17 14:25:19 +0000

    added contents to cell.java: added attributes added 2 constructors added validation method added getter and setter

--- cell.java
+++ cell.java
@@ -1,8 +1,47 @@
 import java.util.Random;
 
 public class cell {
-    
-    //class attributes
-    private int length = 10;
-    private cell[] tape;
+
+    Random random = new Random();
+    //attributes
+    private static final char[] possibleValues = {0, 1};
+    private char value;
+
+    public boolean validVal(char char1) {
+
+        //checks a
+        for (int i = 0; i < possibleValues.length; i++) {
+            if (possibleValues[i] == char1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public cell(){
+        int temp = random.nextInt() % possibleValues.length;
+        this.value = (char)(temp);
+    }
+
+    public cell(char value) {
+        if (validVal(value)) {
+            this.value = value;
+        } else {
+            System.out.println("Invalid value for cell");
+        }
+    }
+
+    //getter and setter for value
+    public void setValue(char value) {
+        if (validVal(value)) {
+            this.value = value;
+        } else {
+            System.out.println("Invalid setting for value");
+        }
+    }
+
+    public char getValue() {
+        return value;
+    }
+
 }
